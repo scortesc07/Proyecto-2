87902253907ad1b9436639dbe93a08301bcf930e
scortesc07 <sebacortes.caceres@gmail.com>
2022-09-17 10:35:26 -0300

    se actualiza estilos, html y js

--- crud.js
+++ crud.js
@@ -1,73 +1,27 @@
-var selectedRow = null
+'use strict'
 
-function onFormSubmit() {
-    if (validate()) {
-        var formData = readFormData();
-        if (selectedRow == null)
-            insertNewRecord(formData);
-        else
-            updateRecord(formData);
-        resetForm();
-    }
+const openModal = () => document.getElementById('modal').classList.add('active')
+const openModal2 = () => document.getElementById('modal2').classList.add('active')
+
+const closeModal2 = () => {
+    document.getElementById('modal2').classList.remove('active')
 }
 
-function readFormData() {
-    var formData = {};
-    formData["nombre"] = document.getElementById("nombre").value;
-    formData["cargo"] = document.getElementById("cargo").value;
-    formData["tarea"] = document.getElementById("tarea").value;
-    return formData;
+const closeModal = () => {
+    document.getElementById('modal').classList.remove('active')
 }
 
-function insertNewRecord(data) {
-    var table = document.getElementById("listaEmpleados").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.nombre;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.cargo;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.tarea;
-    cell3 = newRow.insertCell(3);
-    cell3.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
+const getLocalStorage = () => JSON.parse(localStorage.getItem(bd_producto)) ?? []
+const setLocalStorage = (bd_producto) => localStorage.setItem("bd_producto", JSON.stringify(bd_producto))
+
+const borrarProducto = (index) => {
+    const bdProducto = readProduct()
+    bdProducto.splice(index, 1)
+    setLocalStorage(bdProducto)
 }
 
-function resetForm() {
-    document.getElementById("nombre").value = "";
-    document.getElementById("cargo").value = "";
-    document.getElementById("tarea").value = "";
-    selectedRow = null;
-}
-
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("nombre").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("cargo").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("tarea").value = selectedRow.cells[2].innerHTML;
-}
-function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.nombre;
-    selectedRow.cells[1].innerHTML = formData.cargo;
-    selectedRow.cells[2].innerHTML = formData.tarea;
-}
-
-function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById("listaEmpleados").deleteRow(row.rowIndex);
-        resetForm();
-    }
-}
-function validate() {
-    isValid = true;
-    if (document.getElementById("nombre").value == "") {
-        isValid = false;
-        document.getElementById("fullNameValidationError").classList.remove("hide");
-    } else {
-        isValid = true;
-        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-            document.getElementById("fullNameValidationError").classList.add("hide");
-    }
-    return isValid;
+const guardarProducto = (index, producto) => {
+    const bdProducto = readProduct()
+    bdProducto[index] = producto
+    setLocalStorage(bdProducto)
 }
